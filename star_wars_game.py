ad4ad5cb5c5d0e14de098f735cc84e66d96d20e8
lim -cm <mynameispi3.14159@gmail.com>
2024-10-26 10:58:42 -0400

    display cockpit on the screen Also changed the size of the display

--- star_wars_game.py
+++ star_wars_game.py
@@ -16,9 +16,9 @@ clock = pygame.time.Clock()
 black = pygame.Color(0,0,0)
 white = pygame.Color(255,255,255)
 
-#screen dimentions/setting up screen w size 300x300, background color black
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 780
+#screen dimentions/setting up screen w size 1490 x 1000, background color black
+SCREEN_WIDTH = 1490
+SCREEN_HEIGHT = 1000
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 SCREEN.fill(black)
 pygame.display.set_caption("Star Wars Game")
@@ -39,9 +39,9 @@ def generateStars():
     x_pos = []
     y_pos = []
     radius = []
-    for i in range (0, 1000):
-        x_pos.append(randomNum(0, 1200))
-        y_pos.append(randomNum(0, 780))
+    for i in range (0, 1500):
+        x_pos.append(randomNum(0, SCREEN_WIDTH))
+        y_pos.append(randomNum(0, SCREEN_HEIGHT))
         radius.append(randomNum(1,3))
     return x_pos, y_pos, radius
 
@@ -53,11 +53,17 @@ def draw_stars(x_pos, y_pos, radius):
         pygame.draw.circle(SCREEN, white, (x, y), r)
 
 def create_crosshairs():
-    crosshairs = import_img("pngfiles/crosshairs2.png")  #imports the png for the crosshairs
-    scaled_width = crosshairs.get_width() // 1.2
-    scaled_height = crosshairs.get_height() // 1.2
+    crosshairs = import_img("image_files/crosshairs.png")  #imports the png for the crosshairs
+    scaled_width = crosshairs.get_width() // .7
+    scaled_height = crosshairs.get_height() // .7
     scaled_image = pygame.transform.scale(crosshairs, (scaled_width, scaled_height))
-    
+    return scaled_image
+
+def create_cockpit():
+    cockpit = import_img("image_files/xwing_cockpit.png")  #imports the png for the crosshairs
+    scaled_width = cockpit.get_width() // 1.381
+    scaled_height = cockpit.get_height() // 1.381
+    scaled_image = pygame.transform.scale(cockpit, (scaled_width, scaled_height))
     return scaled_image
 
 '''
@@ -65,9 +71,11 @@ The code below and prior to the main game loop initializes important variables
 '''
 stars_x_pos, stars_y_pos, stars_radii = generateStars()
 
-#pygame.mouse.set_visible(False) TODO: Uncomment when project is done
+pygame.mouse.set_visible(False) #TODO: Uncomment when project is done
 crosshairs = create_crosshairs()
 crosshairs_rect = crosshairs.get_rect()
+cockpit = create_cockpit()
+
 """
 Main game loop
 """
@@ -83,10 +91,11 @@ while True:
     draw_stars(stars_x_pos, stars_y_pos, stars_radii) #draws stars on the screen
     
     
+    #code to display crosshairs
     mouse_x, mouse_y = pygame.mouse.get_pos()
     crosshairs_rect.center = (mouse_x, mouse_y)
     SCREEN.blit(crosshairs, crosshairs_rect)
     
     
-
+    SCREEN.blit(cockpit, (0,220)) #displays the cockpit 
     pygame.display.update()
