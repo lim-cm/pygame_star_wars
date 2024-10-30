ff992131c7ee8db512a7e364cecb217ea1ac3c16
lim -cm <mynameispi3.14159@gmail.com>
2024-10-30 00:06:17 -0400

    add initial functionality for tie fighters also add image files for the tie fighter and lasers

--- star_wars_game.py
+++ star_wars_game.py
@@ -26,7 +26,7 @@ SCREEN.fill(black)
 background_music = "audio_files/star_wars_theme_8_bit.mp3"
 pygame.mixer.music.load(background_music)
 pygame.mixer.music.set_volume(0.25)
-pygame.mixer.music.play(-1)
+#pygame.mixer.music.play(-1)
 pygame.display.set_caption("Star Wars Game")
 
 def import_img(image_path):
@@ -82,6 +82,19 @@ def shoot_laser(x, y, dir_x, dir_y, radius):
     # Draw the laser
     pygame.draw.circle(SCREEN, red, (x, y), radius)
     return x,y, radius
+
+class TieFighter():
+    def create_tie_fighter():
+        tie_fighter = import_img("image_files/tie_fighter.png")  #imports the png for the crosshairs
+        scaled_width = tie_fighter.get_width() // 20
+        scaled_height = tie_fighter.get_height() // 20
+        scaled_image = pygame.transform.scale(tie_fighter, (scaled_width, scaled_height))
+        return scaled_image
+    
+    def display_tie_fighter(tie_fighter):
+        SCREEN.blit(tie_fighter, (500, 200))
+        
+        
 '''
 The code below and prior to the main game loop initializes important variables
 '''
@@ -98,6 +111,9 @@ lasers1 = []
 lasers2 = []
 laser_count = 0
 laser_sound = pygame.mixer.Sound("audio_files/laser_xwing.mp3")
+
+tie_fighters = []
+tie_fighters.append(TieFighter.create_tie_fighter())
 
 '''
 TODO: add cooldown for laser
@@ -170,6 +186,11 @@ while True:
     
     draw_stars(stars_x_pos, stars_y_pos, stars_radii) #draws stars on the screen
     
+    
+    #code to display tie_fighters
+    for tie_fighter in tie_fighters:
+        TieFighter.display_tie_fighter(tie_fighter)
+        
     
     #code to display crosshairs
     mouse_x, mouse_y = pygame.mouse.get_pos()
